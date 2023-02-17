e3c859ad269f8b4c5113d728ff325c4689866e93
Afroman726 <dymitrihawkins@gmail.com>
2023-02-17 15:23:40 -0800

    added initial files

---
+++ Main.js
@@ -0,0 +1,55 @@
+const etchBody = document.querySelector('#etch_Body');  //Container for the Grid
+const sizeValue = document.querySelector('#size_Value');
+const rainbow = document.querySelector('#rainbow');
+
+
+function makerows (size) {
+    etchBody.style.gridTemplateColumns = `repeat(${size}, 1fr)`;  //These two set the css styles to fit block inside grid
+    etchBody.style.gridTemplateRows=`repeat(${size}, 1fr)`;
+     
+        for(i=0; i < (size * size); i++){    //This part makes the sqr of the container (Legnth * Width (duh))
+            let square = document.createElement("div");
+             etchBody.appendChild(square).className = "block";
+            square.addEventListener("mouseover", () =>{     
+                square.style.backgroundColor= 'black';
+               })
+         
+
+        }
+
+
+
+
+}
+makerows(16);
+
+
+const smallButton = document.querySelector('#small');
+smallButton.addEventListener('click', () => {
+    makerows(16);
+});
+
+const medButton = document.querySelector('#med');
+medButton.addEventListener('click', () => {
+    makerows(32);
+});
+
+const largeButton = document.querySelector('#large');
+largeButton.addEventListener('click', () =>{
+    makerows(64);
+})
+
+const stupidButton = document.querySelector('#stupid');     //This is the don't click me button lol
+stupidButton.addEventListener('click', () =>{
+    makerows(128);
+})
+
+const clearAll = document.querySelector('#clear');
+clearAll.addEventListener('click', () => {
+    etchBody.innerHTML = '';
+})
+
+const erase = document.querySelector('#eraser');
+erase.addEventListener('click', () =>{
+    eraser();
+})
---
+++ README.md
@@ -0,0 +1,3 @@
+Etch-a-Sketch:
+
+This program is a fully functioning browser game that plays like a Etch-a-Sketch!
